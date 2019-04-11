f44610a496e7b64c7b191ab7e5229f735d933e8d
Min Thu Wai <minthuwai.min@gmail.com>
2019-04-11 15:16:33 +0630

    reduced geofence radius

--- app/src/main/java/com/example/android/geofencingmvvm/Common.java
+++ app/src/main/java/com/example/android/geofencingmvvm/Common.java
@@ -2,7 +2,7 @@ package com.example.android.geofencingmvvm;
 
 public class Common {
     public static final String DESTINATION_KEY = "destination_key";
-    public static final float RADIUS_IN_METER = 500;
+    public static final float RADIUS_IN_METER = 200;
     public static final String GEOFENCE_STATUS = "GEOFENCE_STATUS";
     public static final String MARKER_SNIPPET = "Long press this info to remove geofence";
     public static final String MARKER_TITLE = "Destination";
